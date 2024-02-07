a8947f6520b9525ab0732c919e8efd1f6630c0d0
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-07 23:02:18 +0900

    섹션2. 프로젝트 생성
    - 프로젝트 생성 및 환경설정

---
+++ src/test/java/springstudy/secondproject/SpringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.secondproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
