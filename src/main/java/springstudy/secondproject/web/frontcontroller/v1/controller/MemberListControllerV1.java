537a6d068ddaf71068cd86ce233d05aae60c44c8
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-09 22:17:11 +0900

    섹션4. 프론트 컨트롤러 도입 - v1
    - 요청 URI에 따른 컨트롤러 분배를 위한 프론트 컨트롤러 적용

---
+++ src/main/java/springstudy/secondproject/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,26 @@
+package springstudy.secondproject.web.frontcontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import springstudy.secondproject.domain.member.Member;
+import springstudy.secondproject.domain.member.MemberRepository;
+import springstudy.secondproject.web.frontcontroller.v1.ControllerV1;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/springstudy/secondproject/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,31 @@
+package springstudy.secondproject.web.frontcontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import springstudy.secondproject.domain.member.Member;
+import springstudy.secondproject.domain.member.MemberRepository;
+import springstudy.secondproject.web.frontcontroller.v1.ControllerV1;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
