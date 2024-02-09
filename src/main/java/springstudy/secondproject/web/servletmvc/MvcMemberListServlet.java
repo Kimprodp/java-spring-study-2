cdfff7a8abf7cdc77eb5e0af08cf185ebe6ff557
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-09 20:59:05 +0900

    섹션3. MVC 패턴 - 적용
    - Servlet을 Controlloer로 JSP를 View로 HttpServletRequest 객체의 내부 데이터 저장소로를 Model로 사용하여 MVC 패턴을 적용
    - WEB-INF 하위 파일은 URL로 직접적으로 접근 불가 (내부에서 호출해야 접근 가능)

---
+++ src/main/java/springstudy/secondproject/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,29 @@
+package springstudy.secondproject.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import springstudy.secondproject.domain.member.Member;
+import springstudy.secondproject.domain.member.MemberRepository;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
